46649fa3ac24f2d1d67ea81546bafefcbe6ac8cc
YS <ys@workiis>
2011-03-16 03:28:41 +0000

    updated

--- imagining-iteratee-io-in-scheme/imagining-iteratee-io-in-scheme.scm
+++ imagining-iteratee-io-in-scheme/imagining-iteratee-io-in-scheme.scm
@@ -1,94 +1,64 @@
-;; cond-handler: a 2-arg proc. called if a condition occurs.
-;; first arg is the condition: need-more-input, unexpected-eof, or any
-;;   other condition (which will probably be passed as-is to the
-;;   enumerator's general condition handler)
-;; second arg is the continuation to call to resume this iteratee. if
-;;   it's nil, then the the whole iteratee will be called if retried. if
-;;   it's not nil, cc will be called with a new chunk.
+;; an iteratee takes a chunk.
+;; returns 3 values:
+;; 1. 'done or 'cont. if 'done, then the following value is the result. if 'cont, a proc that takes a chunk. 
+;; 2. if *eof*, any value will be ignored. if *done*, then the value to return. if *cont*, then resume continuation
+;; 3. the remaining chunk that's not read/used
 
-;; done-token: see the multi-value return comment.
-
-;; chunk: a chunk generated and passed by the enumerator
-
-
-;; multi-value return from an iteratee:
-
-;; first value: done-token | value
-;; done-token is to be returned if iteratee does not want to process anymoref
-;; value is the value to be returned to the enumerator's caller
-
-;; second value: chunk | #f
-;; 'chunk' is the chunk to be used in the next call to iteratee.
-;; '#f' is to indicate to use a nil for the next iteratee call.
-
-
-(define peek-iteratee (cond-handler done-token chunk)
-  (cond 
-   ((nil? chunk)
-    (values done-token #f))
-   
-   ((empty-chunk? chunk)
-    (cond-handler 'need-more-input nil))
-   
-   (else
-    (values (car chunk) chunk))))
-
-
-
-(define head-iteratee (cond-handler done-token chunk)
-  (cond
-   ((nil? chunk)
-    (cond-handler 'unexpected-eof nil))
-
-   ((empty-chunk? chunk)
-    (cond-handler 'need-more-input nil))
-
-   (else
-    (values (car chunk) (cdr chunk)))))
-
-
-(define make-break-iteratee (break-pred)
-  (lambda (cond-handler done-token chunk)
+(define (prelude-break cpred lst)
+  (let loop ((rbefore '()) (after lst))
     (cond
-     ((nil? chunk)
-      (values done-token #f))
-     ((empty-chunk? chunk)
-      (cond-handler 'need-more-input nil))
-     (else
-      (let step ((chunk chunk) (result '()))
-        (call/mv 
-         (prelude-break break-pred chunk)
-         (lambda (before after)
-           (cond
-            ;; the whole chunk matches, so we need to get more
-            ;; until it does not match
-            ((nil? after)
-             (step (call/cc
-                    (lambda (resume) (cond-handler 'need-more-input resume)))
-                   (append result (car break-result))))
-            ;; not all chunk matches, so we are done.
-            (else
-             (values (append result before) after))))))))))
+     ((null? after)
+      (values (reverse rbefore) after))
+     (else 
+      (cond 
+       ((cpred (car after))
+	(values (reverse rbefore) after))
+       (else
+	(loop (cons (car after) rbefore)
+	      (cdr after))))))))))))
+
+(or (equal? (receive (before after) (prelude-break (lambda (x) (> x 5)) '(1 2 3 4 5 5 6 6 1 7 8 9))
+	      (list before after))
+	    '((1 2 3 4 5 5) (6 6 1 7 8 9)))
+    (error "failed"))
+
+(define (make-break-iteratee break-pred)
+  (define (step chunk)
+    (let loop ((acc '()) (chunk chunk))
+      (cond
+       ((eq? #f chunk)
+	(pp (list 'A 'chunk chunk)) (newline)
+	(values 'done acc '()))
+       ((null? chunk)
+	(pp (list 'B 'chunk chunk)) (newline)
+	(values 'cont (lambda (c) (loop acc c)) '()))
+       (else
+	(receive (before after) (prelude-break break-pred chunk)
+	  (cond
+	   ;; need more until after is not empty
+	   ((null? after)
+	    (pp (list 'C 'chunk after)) (newline)
+	    (values 'cont (lambda (c) (loop (append acc before) c)) '()))
+	   (else
+	    (pp (list 'D 'chunk after)) (newline)
+	    (values 'done acc after))))))))
+  step)
+
+  
+      
+(begin
+  (let ((iteratee (make-break-iteratee (lambda (x) (> x 5)))))
+    (let loop ((i 1)
+	       (chunks '((1 2 3 3 3)
+			 (4 4 3 1 5 5)
+			 (5 5)
+			 #f #f )))
+      (pp (list 'Iteration i 'Chunk (car chunks))) (newline)
+      (receive (val1 val2 leftover) (iteratee (car chunks))
+	(pp (list 'val1 val1 'val2 val2 'leftover leftover)) (newline)
+	(if (not (eq? val1 'done))
+	    (loop (+ 1 i) (cons (append leftover (cadr chunks)) (cddr chunks))))))))
+		 
 
 
-(define make-heads-iteratee (str-to-match)
-  (if (equal? str-to-match "")
-      #f
-      (lambda (cond-handler done-token chunk)
-        (cond
-         ((nil? chunk)
-          (values done-token #f))
-         ((empty-chunk? chunk)
-          (cond-handler 'need-more-input nil))
-         (else
-          (let step ((chunk chunk)
-                     (remaining-match str-to-match)
-                     (cnt 0))
-            (cond
-             ((equal? (car remaining-match) (car chunk))
-              (step (call/cc
-                     (lambda (resume) (cond-handler 'need-more-input resume)))
-                    (cdr remaining-match)
-                    (+1 cnt)))
-             (else
-              (values cnt chunk)))))))))
+
